b8091d1205ebd04096b2c5eb14c11b7294b78f1e
Степан <79895077+Ninth7219@users.noreply.github.com>
2021-03-02 13:14:15 +0600

    Add files via upload

---
+++ Taks_1.4.cpp
@@ -0,0 +1,37 @@
+/**
+    Author: Ботвин Степан
+    Group: СБС 001-О-001 / 1
+    Taks#: 1.4
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    setlocale(LC_ALL, "Rus");
+
+    const double Speed=9, Height=15, alfa=35;
+    double time;
+    bool Input_validation;
+
+    do{
+        cout << "Введите время полета в секундах:";
+        Input_validation = scanf ("%lf", &time);
+        if(Input_validation != 1){
+            printf("Input error: input value is not a number\n");
+            getchar();
+        }
+    }while(Input_validation != 1);
+    cout << "\n";
+
+    double Height_from_the_bottom = Speed*time*cos(alfa);
+    Height_from_the_bottom = Height_from_the_bottom > 0 ? Height_from_the_bottom*1 : Height_from_the_bottom*-1;
+
+    double Destance_from_edge_to_cliff = Height + Height_from_the_bottom - (9.81*time*time)/2;
+    Destance_from_edge_to_cliff = Destance_from_edge_to_cliff > 0 ? Destance_from_edge_to_cliff*1 : Destance_from_edge_to_cliff*-1;
+
+    cout << "Высота от дна обрыва = " << Height_from_the_bottom << "\n""Расстояние до края обрыва = " << Destance_from_edge_to_cliff << "\n";
+    return 0;
+}
